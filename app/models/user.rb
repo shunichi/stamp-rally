4e48071463e3a2b9a725a4d0733a61deb8758ff1
Shunichi Ikegami <sike.tm@gmail.com>
2015-06-21 17:55:56 +0900

    ominiauth の auth_hash を保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
 
+  serialize :auth_hash, JSON
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
+      user.auth_hash = auth
       if auth['info']
         user.name = auth[:info][:name].presence || auth[:info][:nickname].presence || auth[:extra][:raw_info][:login]
       end
