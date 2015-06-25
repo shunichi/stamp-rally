7d829c4d5135671e31b1a4ac76a6283aa25aa027
Shunichi Ikegami <sike.tm@gmail.com>
2015-06-25 16:08:12 +0900

    アイコン表示

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625055340) do
+ActiveRecord::Schema.define(version: 20150625064554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150625055340) do
     t.datetime "rally_started_at"
     t.string   "token"
     t.integer  "remotty_entry_id"
+    t.string   "icon_url",                     null: false
   end
 
 end
