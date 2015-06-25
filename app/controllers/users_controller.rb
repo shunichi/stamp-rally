3d29125400b8ed2163de67d77848b85c72a009f5
Shunichi Ikegami <sike.tm@gmail.com>
2015-06-25 17:15:35 +0900

    スタイル調整など

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show)
 
+  def index
+  end
+
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     post '/debug/signin' => 'sessions#debug_signin', as: :debug_signin
   end
 
-  resources :users, only: %i(show) do
+  resources :users, only: %i(index show) do
     patch 'start_rally', on: :collection
     resources :stamps, only: %i(create destroy)
   end
