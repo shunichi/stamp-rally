8cf1d9b6fc59c1561de19c1e3558cc6e5548a595
Shunichi Ikegami <sike.tm@gmail.com>
2015-06-28 02:24:50 +0900

    Stamp に validation を追加

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -3,6 +3,7 @@ class Stamp < ActiveRecord::Base
   belongs_to :master, class_name: 'User'
 
   validates :user_id, uniqueness: { scope: :master_id }
+  validates :user_id, :master_id, presence: true
 
   def self.max_count
     User.master_names.size
