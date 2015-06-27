0d95d26bd62d0412049bd07355e7903eda666a69
Shunichi Ikegami <sike.tm@gmail.com>
2015-06-27 16:43:47 +0900

    user_spec.rb の書き方を少々変えた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,14 @@
 describe User do
-
-  it 'ENV["MASTER_NAMES"] に含まれる名前の場合 master になる' do
-    # master1, master2, master3 は .env.test の MASTER_NAMES に含まれる
-    %w(master1 master2 master3).each do |name|
-      expect(create(:user, name: name).master?).to eq true
+  describe '#user_type' do
+    it 'ENV["MASTER_NAMES"] に含まれる名前の場合 master になる' do
+      # master1, master2, master3 は .env.test の MASTER_NAMES に含まれる
+      %w(master1 master2 master3).each do |name|
+        expect(create(:user, name: name).user_type).to eq 'master'
+      end
     end
-  end
 
-  it 'ENV["MASTER_NAMES"] に含まれない名前の場合 trainee になる' do
-    expect(create(:user).master?).to eq false
+    it 'ENV["MASTER_NAMES"] に含まれない名前の場合 trainee になる' do
+      expect(create(:user).user_type).to eq 'trainee'
+    end
   end
 end
